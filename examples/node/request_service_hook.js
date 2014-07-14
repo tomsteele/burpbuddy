d1a112964a63c0805c8f56179c7f43a006224328
tomsteele <thomasjsteele@gmail.com>
2014-07-14 15:19:20 -0700

    Adds console.log of referenceID

--- examples/node/request_service_hook.js
+++ examples/node/request_service_hook.js
@@ -6,7 +6,9 @@ server.route({
     method: 'POST',
     path: '/request',
     handler: function (req, reply) {
+        console.log(req.payload.referenceID);
         req.payload.headers.beep = 'boop';
+        req.payload.path = '/foo';
         reply(req.payload);
     }
 });
