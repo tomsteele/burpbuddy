5a93ce7995dfd9b820e825567f666d3d2821d06d
tomsteele <thomasjsteele@gmail.com>
2014-07-17 09:41:44 -0700

    Fix null response issue with sitemap

--- burp/src/main/java/burp/BHttpRequestResponseFactory.java
+++ burp/src/main/java/burp/BHttpRequestResponseFactory.java
@@ -6,10 +6,18 @@ public class BHttpRequestResponseFactory {
     public static BHttpRequestResponse create(IHttpRequestResponse requestResponse,
                                               IBurpExtenderCallbacks callbacks, IExtensionHelpers helpers) {
         BHttpRequestResponse bHttpRequestResponse = new BHttpRequestResponse();
-        bHttpRequestResponse.request = BHttpRequestFactory.create(0x00004242, requestResponse,
-                helpers.analyzeRequest(requestResponse), callbacks);
-        bHttpRequestResponse.response = BHttpResponseFactory.create(0x00004242, requestResponse,
-                helpers.analyzeResponse(requestResponse.getResponse()), callbacks);
+
+
+        if (requestResponse.getRequest() != null && requestResponse.getRequest().length > 0) {
+            bHttpRequestResponse.request = BHttpRequestFactory.create(0x00004242, requestResponse,
+                    helpers.analyzeRequest(requestResponse), callbacks);
+        }
+
+        if (requestResponse.getResponse() != null && requestResponse.getResponse().length > 0) {
+            bHttpRequestResponse.response = BHttpResponseFactory.create(0x00004242, requestResponse,
+                    helpers.analyzeResponse(requestResponse.getResponse()), callbacks);
+        }
+
         bHttpRequestResponse.messageType = "requestResponse";
         return bHttpRequestResponse;
     }
